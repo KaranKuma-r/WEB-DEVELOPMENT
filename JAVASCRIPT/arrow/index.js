8a4fa9da03556784275364653ca25c0c71f2b0a0
Karan Kumar <kk5214662@gmail.com>
2024-06-02 05:28:46 +0530

    UPDATE

--- JAVASCRIPT/arrow/index.js
+++ JAVASCRIPT/arrow/index.js
@@ -332,11 +332,11 @@
 // Use destructuring to find the employee with the highest salary. Return the employee object with the highest salary.
 // Example Invocation:
 
-const employees = [
-    { name: "John Doe", age: 30, department: "HR", salary: 50000 },
-    { name: "Jane Smith", age: 28, department: "Finance", salary: 60000 },
-    { name: "Alex Johnson", age: 35, department: "IT", salary: 70000 },
-];
+// const employees = [
+//     { name: "John Doe", age: 30, department: "HR", salary: 50000 },
+//     { name: "Jane Smith", age: 28, department: "Finance", salary: 60000 },
+//     { name: "Alex Johnson", age: 35, department: "IT", salary: 70000 },
+// ];
 
 // // // console.log(highestPaid(employees)); // Output: { name: 'Alex Johnson', age: 35, department: 'IT', salary: 70000 }
 
